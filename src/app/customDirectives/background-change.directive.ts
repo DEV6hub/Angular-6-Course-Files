872916367fd23edcfcf2fd71e25b88efec7588a0
gagankdev6 <gagank@dev6.com>
2018-07-10 15:08:33 -0400

    step2

--- src/app/customDirectives/background-change.directive.ts
+++ src/app/customDirectives/background-change.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, ElementRef, Renderer, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBackgroundChange]'
 })
 export class BackgroundChangeDirective {
-
-  constructor() { }
+  @Input() color: string;
+  constructor(private el: ElementRef, private renderer: Renderer) {
+  }
+  @HostListener('mouseenter') onMouseEnter() {
+   this.highlight(this.color || 'red');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+   this.highlight(null);
+  }
+  private highlight(color: string) {
+   this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }

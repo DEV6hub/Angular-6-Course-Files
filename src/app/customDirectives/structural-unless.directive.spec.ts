420c592908dd6f73a510108c2c682ecaf2431614
gagankdev6 <gagank@dev6.com>
2018-07-10 15:18:38 -0400

    step4

---
+++ src/app/customDirectives/structural-unless.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StructuralUnlessDirective } from './structural-unless.directive';
+
+describe('StructuralUnlessDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StructuralUnlessDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/customDirectives/structural-unless.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appStructuralUnless]'
+})
+export class StructuralUnlessDirective {
+
+  constructor() { }
+
+}

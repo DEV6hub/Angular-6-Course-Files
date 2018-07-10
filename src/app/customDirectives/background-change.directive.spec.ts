267773209a6ebf9743619c0fb2c70ace9b943a25
gagankdev6 <gagank@dev6.com>
2018-07-10 15:00:55 -0400

    step1

---
+++ src/app/customDirectives/background-change.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BackgroundChangeDirective } from './background-change.directive';
+
+describe('BackgroundChangeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BackgroundChangeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/customDirectives/background-change.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBackgroundChange]'
+})
+export class BackgroundChangeDirective {
+
+  constructor() { }
+
+}

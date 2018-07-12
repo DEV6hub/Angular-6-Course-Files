b85b968a42f7d8aebf8e4262924289a400526c1a
gagankdev6 <gagank@dev6.com>
2018-07-12 08:16:23 -0400

    step4

--- src/app/style-gallery/style-gallery.module.ts
+++ src/app/style-gallery/style-gallery.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 import { AllStyleOptionsComponent } from './all-style-options/all-style-options.component';
 import { AllGraphicsComponent } from './all-graphics/all-graphics.component';
 import { StyleColorsComponent } from './style-colors/style-colors.component';
 import { GraphicComponent } from './graphic/graphic.component';
-
+import { UserInfoService } from '../core/user-info.service';
+const routes: Routes = [
+  {
+    path: '',
+    component: AllStyleOptionsComponent,
+    children: [
+      {path: 'allGraphics', component: AllGraphicsComponent,
+      children: [
+        {path: 'graphic/:graphicName', component: GraphicComponent}
+      ]},
+      {path: 'allColors', component: StyleColorsComponent},
+    ]
+  }
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ],
-  declarations: [AllStyleOptionsComponent, AllGraphicsComponent, StyleColorsComponent, GraphicComponent]
+  declarations: [
+    AllStyleOptionsComponent,
+    AllGraphicsComponent,
+    StyleColorsComponent,
+    GraphicComponent
+  ],
+  providers: [
+    UserInfoService
+  ],
 })
 export class StyleGalleryModule { }

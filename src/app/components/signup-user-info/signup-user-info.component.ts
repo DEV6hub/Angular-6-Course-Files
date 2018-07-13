001940fff927910cb437e1bef468f9c5c0e2790e
gagankdev6 <gagank@dev6.com>
2018-07-13 09:32:20 -0400

    Upgraded to Angular6

--- src/app/components/signup-user-info/signup-user-info.component.ts
+++ src/app/components/signup-user-info/signup-user-info.component.ts
@@ -49,9 +49,10 @@ export class SignupUserInfoComponent implements OnInit {
 
   save(userInfo) {
     if (this.userInfoForm.valid) {
-      //this.userInfo = new UserInfo(userInfo);
       this.userInfoService.addUser(userInfo);
       this.router.navigateByUrl('/catalog');
+    } else {
+      window.alert('Please fill up enitre form correctly before pressing Save button.');
     }
   }
 }
